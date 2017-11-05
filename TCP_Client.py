5f84540b62b76d80228576d031a09622068ddd0d
Beastea3 <yzhao9481@gmail.com>
2017-11-05 16:30:42 +0800

    Description added

--- TCP_Client.py
+++ TCP_Client.py
@@ -1,8 +1,9 @@
+# import module
 from socket import *
 
 target_ip = "127.0.0.1"
 target_port = 9999
-
+# socket(socket_family, socket_type, protocol=0(default))
 client = socket(AF_INET, SOCK_STREAM)
 
 client.connect((target_ip, target_port))
