6b431d014f5f52efc7cc035905352a4d032fb8c4
Beastea3 <yzhao9481@gmail.com>
2017-11-05 16:23:33 +0800

    A bit change...

--- TCP_Client.py
+++ TCP_Client.py
@@ -1,9 +1,9 @@
-import socket
+from socket import *
 
 target_ip = "127.0.0.1"
 target_port = 9999
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket(AF_INET, SOCK_STREAM)
 
 client.connect((target_ip, target_port))
 
--- TCP_Server.py
+++ TCP_Server.py
@@ -1,10 +1,10 @@
-import socket
+from socket import *
 import threading
 
 bind_ip = "0.0.0.0"
 bind_port = 9999
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = socket(AF_INET, SOCK_STREAM)
 
 server.bind((bind_ip, bind_port))
 
